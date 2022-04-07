9e7c5d7aba674127b760889d28704a6e3070fcc2
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-07 02:59:25 +0200

    create tuple with old c sharp and new c sharp

---
+++ Tuple in CSharp 7.0/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple_in_CSharp_7._0
+{
+    internal class Animal
+    {
+        public static Tuple<string, double, double> OldAnimalTuple()
+        {
+            string name = "Cat";
+            double weight = 10.5;
+            double height = 5.2;
+            Tuple<string, double, double> activity = new Tuple<string, double, double>(name, weight, height);
+            return activity;
+        }
+        public static (string name, double weight, double height) NewAnimal()
+        {
+            string name = "Dog";
+            double weight = 15;
+            double height = 10;
+            return (name, weight, height);
+        }
+    }
+}
---
+++ Tuple in CSharp 7.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tuple_in_CSharp_7._0;
+
+namespace TupleinCSharp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = Animal.OldAnimalTuple();
+            Console.WriteLine($"My Name is {cat.Item1} Weight is {cat.Item2} height is {cat.Item3}");
+            Console.WriteLine("****************************************************************");
+            var dog = Animal.NewAnimal();
+            Console.WriteLine($"My Name is {dog.name} Weight is {dog.weight} height is {dog.height}");
+
+        }
+    }
+}
